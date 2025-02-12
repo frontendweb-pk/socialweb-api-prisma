8bb159fedaab4736543655dd520bb6bc1f1b8e1b
frontendweb-sg <frontendweb.sg@gmail.com>
2025-02-12 14:25:57 +0530

    update vue project

---
+++ clients/socialweb-vue/src/pages/admin/permission/permission.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Permission</h1>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style>
+</style>
--- clients/socialweb-vue/src/router/index.ts
+++ clients/socialweb-vue/src/router/index.ts
@@ -6,11 +6,34 @@ import {
 
 const routes: RouteRecordRaw[] = [
   {
+    path: "/",
+    name: "home",
+    component: () =>
+      import(/* webpackChunkName: "home" */ "@/pages/home/home.vue"),
+  },
+
+  {
     path: "/auth",
     children: [
       {
         path: "",
         component: () => import("@/pages/auth/login/login.vue"),
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    redirect: "/admin/dashboard",
+    name: "admin",
+    children: [
+      {
+        path: "dashboard",
+        component: () => import("@/pages/admin/dashboard/dashboard.vue"),
+      },
+      {
+        path: "role",
+        name: "role",
+        component: () => import("@/pages/admin/role/role.vue"),
       },
     ],
   },
